ba23c9dbf97c9027a6db23cae964a3056b961531
Manuel G <manuel.geissberger@gmail.com>
2022-08-17 22:39:58 +0200

    hide / show Passed tests

--- test_framework/index.js
+++ test_framework/index.js
@@ -1,13 +1,29 @@
 const test_output = window.test_output.split(window.lua_output_new_line);
 const testOutputElement = document.getElementById("test-output-element");
 
-test_output.forEach(log_line => {
-    const isError = log_line.includes("error")
-    
-    const logLineElement = document.createElement("p")
+const showPassed = document.getElementById("show-passed");
 
-    isError ? logLineElement.classList.add("error-line") : logLineElement.classList.add("info-line");
+function updateLog(show_passed) {
+    testOutputElement.innerHTML="";
+    test_output.forEach(log_line => {
+        const isError = log_line.includes("error")
+        const isPassed = log_line.includes("[PASS] -> ")
+        if ((isPassed && show_passed==true) || isError) {
+            const logLineElement = document.createElement("p")
+        
+            isError && logLineElement.classList.add("error-line");
+            isPassed && logLineElement.classList.add("info-line"); 
+        
+            logLineElement.innerText = log_line;
+            testOutputElement.appendChild(logLineElement)
+        }
+        
+    });
+}
 
-    logLineElement.innerText = log_line;
-    testOutputElement.appendChild(logLineElement)
-});
+showPassed.addEventListener('change', (event) => {
+    updateLog(event.target.checked);
+})
+
+updateLog(true);
+
