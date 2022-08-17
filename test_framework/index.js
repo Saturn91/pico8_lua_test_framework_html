912e178a6a97209949c10538c173202530436424
Manuel G <manuel.geissberger@gmail.com>
2022-08-17 21:41:35 +0200

    proof of concept

---
+++ test_framework/index.js
@@ -0,0 +1,15 @@
+const test_output = window.test_output.split(window.lua_output_new_line);
+console.log(test_output)
+
+const testOutputElement = document.getElementById("test-output-element");
+
+test_output.forEach(log_line => {
+    const isError = log_line.includes("error")
+    
+    const logLineElement = document.createElement("p")
+
+    isError ? logLineElement.classList.add("error-line") : logLineElement.classList.add("info-line");
+
+    logLineElement.innerText = log_line;
+    testOutputElement.appendChild(logLineElement)
+});
